5583fe4b8968fe65fb0b7110440977febce36e64
ishika3189beai25-code <ishika3189.beai25@chitkara.edu.in>
2026-07-18 17:05:27 +0530

    Time: 45 ms (26.29%), Space: 46.9 MB (84.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int sum =0;
         for(int i=0;i<nums.length;i++){
-           for(int j=i+1;j<nums.length;j++) {
-            if(nums[i]+nums[j]==target){
-                return new int[]{i,j};
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
             }
-           }
-        }return new int[]{};
+        }return new int[]{-1, -1};
     }
 }
