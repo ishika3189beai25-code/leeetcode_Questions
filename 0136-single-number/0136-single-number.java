f63d0c7222d5170d703e5f95306e9e7555192f3a
ishika3189beai25-code <ishika3189.beai25@chitkara.edu.in>
2026-07-21 11:55:24 +0530

    Time: 1 ms (99.96%), Space: 46.6 MB (92.86%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,14 @@
 class Solution {
     public int singleNumber(int[] nums) {
-    Arrays.sort(nums);
-    for(int i=0;i<nums.length-1;i+=2){
-        if(nums[i]!=nums[i+1]){
-            return nums[i];
-        }
-    } return nums[nums.length - 1];
+    // Arrays.sort(nums);
+    // for(int i=0;i<nums.length-1;i+=2){
+    //     if(nums[i]!=nums[i+1]){
+    //         return nums[i];
+    //     }
+    // } return nums[nums.length - 1];
+    int sol =0;
+    for(int i=0;i<nums.length;i++){
+        sol = sol^nums[i];
+    }return sol;
     }
 }
